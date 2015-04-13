dba35ca4c8217bddb9287c2024a52816ae966871
Shadowself <zgy1206@163.com>
2015-04-13 22:48:00 +0800

    从服务端获取数据，实现修改，删除等功能！

--- app/src/main/java/com/zgy/graduation/graduationproject/activity/findPestKindActivity.java
+++ app/src/main/java/com/zgy/graduation/graduationproject/activity/findPestKindActivity.java
@@ -26,14 +26,13 @@ import java.io.IOException;
 /**
  * Created by zhangguoyu on 2015/4/7.
  */
-public class findPestKindActivity extends BaseActivity implements View.OnClickListener{
+public class findPestKindActivity extends BaseActivity implements View.OnClickListener {
     private static final String TAG = LoginActivity.class.getSimpleName();
 
     private ImageView pestPicture;
     private Button choose_way;
-    Bitmap newBitmap;
 
-    private static final String IMAGE_FILE_LOCATION = "file:///sdcard/temp.jpg";// temp
+    private static final String IMAGE_FILE_LOCATION = "file:///sdcard/tempPicture.jpg";// temp
     // file
     Uri imageUri = Uri.parse(IMAGE_FILE_LOCATION);// The Uri to store the big
     // bitmap
@@ -48,33 +47,16 @@ public class findPestKindActivity extends BaseActivity implements View.OnClickLi
         comm_title.setText(getString(R.string.find_pest_kind));
         back_main.setVisibility(View.VISIBLE);
 
-        pestPicture = (ImageView)findViewById(R.id.pestPicture);
-        choose_way = (Button)findViewById(R.id.choose_way);
+        pestPicture = (ImageView) findViewById(R.id.pestPicture);
+        choose_way = (Button) findViewById(R.id.choose_way);
         choose_way.setOnClickListener(this);
 
-    }
-
-    // 对相机返回的相片进行裁减
-    private void cropImageUri(Uri uri, int outputX, int outputY, int requestCode) {
-        Intent intent = new Intent("com.android.camera.action.CROP");
-        intent.setDataAndType(uri, "image/*");
-        intent.putExtra("crop", "true");
-        intent.putExtra("aspectX", 1);
-        intent.putExtra("aspectY", 1);
-        intent.putExtra("outputX", outputX);
-        intent.putExtra("outputY", outputY);
-        intent.putExtra("scale", true);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
-        intent.putExtra("return-data", false);
-        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
-        intent.putExtra("noFaceDetection", true); // no face detection
-        startActivityForResult(intent, requestCode);
     }
 
 
     //保存到sd卡
     public static void savePhotoToSDCard(String path, String photoName,
-                                         Bitmap photoBitmap) {
+        Bitmap photoBitmap) {
         if (android.os.Environment.getExternalStorageState().equals(
                 android.os.Environment.MEDIA_MOUNTED)) {
             File dir = new File(path);
@@ -121,31 +103,25 @@ public class findPestKindActivity extends BaseActivity implements View.OnClickLi
                     if (data != null) {
 
                         Bitmap bp = data.getParcelableExtra("data");
-                        if(null == bp){
+                        if (null == bp) {
                             ContentResolver resolver = getContentResolver();
-                        //照片的原始资源地址
+                            //照片的原始资源地址
                             Uri originalUri = data.getData();
                             bp = decodeUriAsBitmap(originalUri);
                         }
                         pestPicture.setImageBitmap(bp);
-
-                        savePhotoToSDCard(userPhoto, "image.jpg", bp);
+                        keepPicture(bp);
+//                        savePhotoToSDCard(userPhoto, "image.jpg", bp);
                     } else {
                         Log.e(TAG, "CHOOSE_SMALL_PICTURE: data = " + data);
                     }
                     break;
-
                 case 1:
-                    Log.i(TAG, "TAKE_SMALL_PICTURE: data = " + data);
-                    // TODO sent to crop
-                    cropImageUri(imageUri, 400, 400, 2);
-
-                    break;
-                case 2:
                     if (imageUri != null) {
                         Bitmap bmp = decodeUriAsBitmap(imageUri);
                         pestPicture.setImageBitmap(bmp);
-                        savePhotoToSDCard(userPhoto, "image.jpg", bmp);
+//                        savePhotoToSDCard(userPhoto, "image.jpg", bmp);
+                        keepPicture(bmp);
                     } else {
                         Log.e(TAG, "CROP_SMALL_PICTURE: data = " + data);
                     }
@@ -171,11 +147,6 @@ public class findPestKindActivity extends BaseActivity implements View.OnClickLi
         return bitmap;
     }
 
-    private Uri getImageUri() {
-        return Uri.fromFile(new File(Environment.getExternalStorageDirectory(),
-                IMAGE_FILE_NAME));
-    }
-
     public void createDialog() {
         new AlertDialog.Builder(mContext)
                 .setItems(items, new DialogInterface.OnClickListener() {
@@ -183,20 +154,13 @@ public class findPestKindActivity extends BaseActivity implements View.OnClickLi
                     public void onClick(DialogInterface dialog, int which) {
                         switch (which) {
                             case 0:
-                                //調用相冊
-//                                Intent intentFromCapture = new Intent(Intent.ACTION_GET_CONTENT, null);
-//                                intentFromCapture.setType("image/*");
-//                                intentFromCapture.putExtra("crop", "true");
-//                                intentFromCapture.putExtra("aspectX", 1);
-//                                intentFromCapture.putExtra("aspectY", 1);
-//                                intentFromCapture.putExtra("outputX", 200);
-//                                intentFromCapture.putExtra("outputY", 200);
-//                                intentFromCapture.putExtra("scale", true);
-//                                intentFromCapture.putExtra("return-data", true);
-//                                intentFromCapture.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
-//                                intentFromCapture.putExtra("noFaceDetection", true); // no face detection
-//                                startActivityForResult(intentFromCapture, 0);
+                                Intent intentFromGallery = new Intent();
+                                intentFromGallery.setType("image/*"); // 设置文件类型
+                                intentFromGallery.setAction(Intent.ACTION_GET_CONTENT);
+                                startActivityForResult(intentFromGallery, 0);
 
+                                break;
+                            case 1:
                                 Intent intentFromCapture = new Intent(
                                         MediaStore.ACTION_IMAGE_CAPTURE);
                                 // 判断存储卡是否可以用，可用进行存储
@@ -207,19 +171,8 @@ public class findPestKindActivity extends BaseActivity implements View.OnClickLi
                                                 .getExternalStorageDirectory(),
                                                 IMAGE_FILE_NAME)));
 
-                                startActivityForResult(intentFromCapture,0);
-                                break;
-                            case 1:
-                                //调用相机
-//                                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);// action is
-//                                // capture
-//                                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-//                                startActivityForResult(intent, 2);// or TAKE_SMALL_PICTURE
+                                startActivityForResult(intentFromCapture, 1);
 
-                                Intent intentFromGallery = new Intent();
-                                intentFromGallery.setType("image/*"); // 设置文件类型
-                                intentFromGallery.setAction(Intent.ACTION_GET_CONTENT);
-                                startActivityForResult(intentFromGallery,2);
 
                                 break;
                         }
@@ -237,10 +190,29 @@ public class findPestKindActivity extends BaseActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.choose_way:
-                    createDialog();
+                createDialog();
                 break;
         }
+    }
+
+    public void keepPicture(Bitmap photo) {
+        FileOutputStream b = null;
+        String userPhoto = String.format("%sphoto%s%s.jpg", DeviceUtil.getSDcardDir() + DeviceUtil.DEFAULTBASEPATH,
+                File.separator,
+                "temp");
+        File file = new File(userPhoto);
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();// 创建文件夹
+        }
+        try {
+            b = new FileOutputStream(new File(userPhoto));
+            photo.compress(Bitmap.CompressFormat.JPEG, 100, b);// 把数据写入文件
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
